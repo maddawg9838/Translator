f1c85fb21a7c2741477d77e7dcacc23f777c0518
maddawg9838 <125151938+maddawg9838@users.noreply.github.com>
2023-05-30 10:34:06 -0500

    Update main.cpp
    Finished and working

--- main.cpp
+++ main.cpp
@@ -9,64 +9,86 @@ void ToASCII();
 
 int main()
 {
-  string translation;
-  
-  cout << "Do you want to convert to a string or to ACII?" << endl;
-  cin >> translation;
-  
-  if (translation == "string")
-  {
-    ToString();
-  }
-  else if (translation == "ASCII")
-  {
-    ToASCII();
-  }
-  else
-  {
-    cout << "Do you want to convert to a string or to ACII?" << endl;
+    string translation;
+
+    cout << "Do you want to convert to a string or to ASCII?" << endl;
     cin >> translation;
-  }
+
+    if (translation == "string")
+    {
+        ToString();
+    }
+    else if (translation == "ASCII")
+    {
+        ToASCII();
+    }
+    else
+    {
+        cout << "Do you want to convert to a string or to ASCII?" << endl;
+        cin >> translation;
+    }
 }
 
 void ToString()
 {
-  int size;
-  
-  cout << "How long is your message?" << endl;
-  cin >> size;
+    int size;
+
+    cout << "How long is your message?" << endl;
+    cin >> size;
 
     int message[size];
     char newMessage[size];
 
-  cout << "What is your message?" << endl;
+    cout << "What is your message?" << endl;
 
-for (int i = 0; i < size; i++)
-  {
-    cin >> message[i];
-    newMessage[i] = char(message[i]);
-  }
-  
-  cout << "Your message: ";
-  for (int i = 0; i < size; i++)
-  {
-    cout << message[i];
-  }
-    cout << " in ASCII is the following: ";
-     for (int i = 0; i < size; i++)
-  {
-    cout << newMessage[i];
-  }  
+    for (int i = 0; i < size; i++)
+    {
+        cout << "What is the " << (i + 1) << " number in the message: ";
+        cin >> message[i];
+        newMessage[i] = char(message[i]);
+    }
+
+    cout << "Your message: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << message[i] << " ";
+    }
+
+    cout << " as a string is the following: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << newMessage[i];
+    }
 }
 
 void ToASCII()
 {
-  string message, newMessage;
-  
-  cout << "What is your message?" << endl;
-  getline(cin, message);
-  
-  newMessage = stoi(message);
-  
-  cout << "Your message: " << message << " in ASCII is the following: " << newMessage;  
+    int size;
+
+    cout << "How long is your message?" << endl;
+    cin >> size;
+
+    int newMessage[size];
+    char message[size];
+
+    cout << "What is your message?" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "What is the " << (i + 1) << " number in the message: ";
+        cin >> message[i];
+        newMessage[i] = int(message[i]);
+    }
+
+    cout << "Your message: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << message[i];
+    }
+    cout << " in ASCII is the following: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << newMessage[i] <<  " ";
+    }
 }
+
